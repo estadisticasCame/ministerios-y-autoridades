56fb645ced2d0c0256df090345e1e0c3dcbbf3d0
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-12-26 16:47:22 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -59,7 +59,7 @@ def pagina_gobiernos_provinciales():
         data = hojas[opcion_seleccionada]
         st.dataframe(data)
         #data["CONCATENACION"] = data["TRATAMIENTO"] + " " + data["NOMBRE"] + " " + data["APELLIDO"] 
-        st.image("imgs/javier_milei")
+        st.image("imgs/javier_milei.png")
 
 
 # Crear columnas
