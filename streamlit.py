723ad0a7f771bf6c18c03bbd8ce0d22957634791
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-12-26 13:40:46 -0300

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import streamlit as st
+
+columna1, columna2 = st.columns(2)
+with columna1:
+    st.title("Tablero de control")
+    st.header("Ministerios y Autoridades")
+    
+with columna2:
+    st.image("imgs/escudo.png")
+
+# Realizamos la carga de datos y lo guardamos en cache de streamlit
+@st.cache_data
+def cargar_datos_excel():
+    # Ingestamos el archivo de excel del meppi
+    excel = pd.read_excel("Datos/Ministerios y autoridades.xlsx", sheet_name=None)
+    # Almacenamos las hojas en un diccionario de Pandas
+    hojas = {}
+    for nombre_hoja, datos_hoja in excel.items():
+        hojas[nombre_hoja] = datos_hoja
+
+    return hojas
+
+hojas = cargar_datos_excel()
