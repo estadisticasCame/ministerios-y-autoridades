0cea6c201488aabd94cbf4fbc6aa04533b344dec
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-12-26 15:12:49 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -30,49 +30,44 @@ def cargar_datos_excel():
 
 hojas, nombre_hojas = cargar_datos_excel()
 
-def gobierno_nacional():
-    # HOJAS GOBIERNO NACIONAL
-    presidencia_de_la_nacion = hojas[nombre_hojas[1]]
-    jefatura_gabinete = hojas[nombre_hojas[2]]
-    ministerio_defensa = hojas[nombre_hojas[3]]
-    ministerio_justicia = hojas[nombre_hojas[4]]
-    ministerio_seguridad = hojas[nombre_hojas[5]]
-    ministerio_interior = hojas[nombre_hojas[6]]
-    ministerio_salud = hojas[nombre_hojas[7]]
-    ministerio_relaciones_exteriores = hojas[nombre_hojas[8]]
-    ministerio_economia = hojas[nombre_hojas[9]]
-    ministerio_capital_humano = hojas[nombre_hojas[10]]
-    ministerio_infraestructura = hojas[nombre_hojas[11]]
-    diputados = hojas[nombre_hojas[12]]
-    senadores = hojas[nombre_hojas[13]]
-    gobernadores = hojas[nombre_hojas[14]]
-
-def gobiernos_provinciales():
-    # HOJAS GOBIERNO PROVINCIAL
-    buenos_aires = hojas[nombre_hojas[15]]
-    caba = hojas[nombre_hojas[16]]
-    catamarca = hojas[nombre_hojas[17]]
-    chaco = hojas[nombre_hojas[18]]
-    chubut = hojas[nombre_hojas[19]]
-    cordoba = hojas[nombre_hojas[20]]
-    corrientes = hojas[nombre_hojas[21]]
-    entrerios = hojas[nombre_hojas[22]]
-    formosa = hojas[nombre_hojas[23]]
-    jujuy = hojas[nombre_hojas[24]]
-    lapampa = hojas[nombre_hojas[25]]
-    larioja = hojas[nombre_hojas[26]]
-    mendoza = hojas[nombre_hojas[27]]
-    misiones = hojas[nombre_hojas[28]]
-    neuquen = hojas[nombre_hojas[29]]
-    rionegro = hojas[nombre_hojas[30]]
-    salta = hojas[nombre_hojas[31]]
-    sanjuan = hojas[nombre_hojas[32]]
-    sanluis = hojas[nombre_hojas[33]]
-    santacruz = hojas[nombre_hojas[34]]
-    santafe = hojas[nombre_hojas[35]]
-    santiagodelestero = hojas[nombre_hojas[36]]
-    tierradelfuego = hojas[nombre_hojas[37]]
-    tucuman = hojas[nombre_hojas[38]]
+presidencia_de_la_nacion = hojas[nombre_hojas[1]]
+jefatura_gabinete = hojas[nombre_hojas[2]]
+ministerio_defensa = hojas[nombre_hojas[3]]
+ministerio_justicia = hojas[nombre_hojas[4]]
+ministerio_seguridad = hojas[nombre_hojas[5]]
+ministerio_interior = hojas[nombre_hojas[6]]
+ministerio_salud = hojas[nombre_hojas[7]]
+ministerio_relaciones_exteriores = hojas[nombre_hojas[8]]
+ministerio_economia = hojas[nombre_hojas[9]]
+ministerio_capital_humano = hojas[nombre_hojas[10]]
+ministerio_infraestructura = hojas[nombre_hojas[11]]
+diputados = hojas[nombre_hojas[12]]
+senadores = hojas[nombre_hojas[13]]
+gobernadores = hojas[nombre_hojas[14]]
+buenos_aires = hojas[nombre_hojas[15]]
+caba = hojas[nombre_hojas[16]]
+catamarca = hojas[nombre_hojas[17]]
+chaco = hojas[nombre_hojas[18]]
+chubut = hojas[nombre_hojas[19]]
+cordoba = hojas[nombre_hojas[20]]
+corrientes = hojas[nombre_hojas[21]]
+entrerios = hojas[nombre_hojas[22]]
+formosa = hojas[nombre_hojas[23]]
+jujuy = hojas[nombre_hojas[24]]
+lapampa = hojas[nombre_hojas[25]]
+larioja = hojas[nombre_hojas[26]]
+mendoza = hojas[nombre_hojas[27]]
+misiones = hojas[nombre_hojas[28]]
+neuquen = hojas[nombre_hojas[29]]
+rionegro = hojas[nombre_hojas[30]]
+salta = hojas[nombre_hojas[31]]
+sanjuan = hojas[nombre_hojas[32]]
+sanluis = hojas[nombre_hojas[33]]
+santacruz = hojas[nombre_hojas[34]]
+santafe = hojas[nombre_hojas[35]]
+santiagodelestero = hojas[nombre_hojas[36]]
+tierradelfuego = hojas[nombre_hojas[37]]
+tucuman = hojas[nombre_hojas[38]]
 
 
 
@@ -83,13 +78,14 @@ columna3, columna4 = st.columns(2)
 with columna3:
     if st.button("GOBIERNO NACIONAL",use_container_width = True):
         hojas_gob_nacional = nombre_hojas[1:15]
-        gobierno_nacional()
-        st.selectbox("Seleccioná",hojas_gob_nacional )
+        opcion_seleccionada = st.selectbox("Seleccioná",hojas_gob_nacional )
         
 
     
 with columna4:
     if st.button("GOBIERNOS PROVINCIALES",use_container_width = True):
         hojas_gob_prov = nombre_hojas[15:]
-        gobiernos_provinciales()
-        st.selectbox("Seleccioná",hojas_gob_prov )
+        opcion_seleccionada = st.selectbox("Seleccioná",hojas_gob_prov )
+    
+if opcion_seleccionada ==  hojas_gob_nacional[0] 
+    st.dataframe(presidencia_de_la_nacion)
