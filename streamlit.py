af3862e1098291e728a92ee9f0426bfb3f6ea8fd
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-12-27 10:50:36 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -45,8 +45,7 @@ def pagina_gobierno_nacional():
     st.session_state.estado['seleccion_desplegable'] = opcion_seleccionada
     if opcion_seleccionada != "-":
         data = hojas[opcion_seleccionada]
-        #data = data.loc[:, ~data.columns.duplicated()]
-        #st.dataframe(data)
+        data = data.loc[:, ~data.columns.duplicated()]
         try:
             data["CONCATENACION"] = data["TRATAMIENTO"] + " " + data["NOMBRE"] + " " + data["APELLIDO"] 
         except:
