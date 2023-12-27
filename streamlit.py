b89d578d6d7ba17c37424db5a969f0a5c627417d
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-12-27 09:01:25 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -76,7 +76,7 @@ def pagina_gobiernos_provinciales():
         st.dataframe(data)
         # Crear una lista de contenedores para imágenes y texto asociado
         # Crear una lista de contenedores para imágenes y texto asociado
-        for texto1, texto2 in zip(data["CONCATENACION"], data["ENTE"]):
+        for texto1, texto2, texto3, texto4 in zip(data["CONCATENACION"], data["ENTE"], data["EMAIL 1"] + data["TELEFONO1"]):
             with st.container(border=True):
                 # Crear columnas dentro del contenedor
                 col_imagen, col_texto = st.columns([1, 2])
@@ -85,6 +85,8 @@ def pagina_gobiernos_provinciales():
                 col_imagen.image("imgs/javier_milei.png")
                 col_texto.write(f"**{texto1}**")
                 col_texto.write(f"{texto2}")
+                col_texto.write(f"{texto3}")
+                col_texto.write(f"{texto4}")
 
 
 
