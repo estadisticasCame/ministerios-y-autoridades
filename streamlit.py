ab760ce77088c74ceae47bd957bfddf46859fea0
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-12-27 09:11:50 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -60,11 +60,8 @@ def pagina_gobierno_nacional():
                 # Mostrar la imagen en la primera columna
                 col_imagen.image("imgs/javier_milei.png")
                 # Aplica estilo solo a la columna de texto
-                # Aplica estilo solo a la columna de texto
-                # Aplica estilo solo a la columna de texto
-                # Aplica estilo solo a la columna de texto
                 col_texto.markdown(
-                    f"<div style='line-height: 1.5; font-size: 16px;'>"
+                    f"<div style='line-height: 1.6; font-size: 17px;'>"
                     f"<strong>{texto1}</strong><br>{texto2}<br>{texto3}<br>{texto4}"
                     "</div>",
                     unsafe_allow_html=True
