3a7fa8258ec544929412f1ddc031ccdf9963668f
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2024-01-18 10:40:35 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -63,16 +63,13 @@ def cargar_datos_excel():
             df = df.copy()
             df["IMAGEN_PREPROCESADA"] = None
             df = df.reset_index(drop=True)
-            st.dataframe(df["URL IMAGEN"])
-            try:
-                for i, imagen5 in enumerate(df["URL IMAGEN"]):
-                    imagen5 = imagen5 if not pd.isna(imagen5) else "https://github.com/estadisticasCame/ministerios-y-autoridades/blob/main/imgs/persona%20no%20encontrada.png"
-                    if "github" in imagen5:
-                        imagen5 = "https://raw.githubusercontent.com/estadisticasCame/ministerios-y-autoridades/main/" + imagen5[72:]
-                    else:
-                        pass    
-            except:
-                pass
+            for i, imagen5 in enumerate(df["URL IMAGEN"]):
+                imagen5 = imagen5 if not pd.isna(imagen5) else "https://github.com/estadisticasCame/ministerios-y-autoridades/blob/main/imgs/persona%20no%20encontrada.png"
+                if "github" in imagen5:
+                    imagen5 = "https://raw.githubusercontent.com/estadisticasCame/ministerios-y-autoridades/main/" + imagen5[72:]
+                else:
+                    pass    
+            
                 response = requests.get(imagen5)
                 # Verificar si la descarga fue exitosa
                 if response.status_code == 200:
